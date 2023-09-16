44bf6a57a4557dcd4b3ca143a4187874125e78d9
DunaevDS <dmitryesfo@yandex.ru>
2023-09-16 16:07:26 +0500

    style: fix codestyle for git tests

--- src/test/java/ru/practicum/shareit/UserControllerTest.java
+++ src/test/java/ru/practicum/shareit/UserControllerTest.java
@@ -19,164 +19,164 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 @SpringBootTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class UserControllerTest {
-	private UserDto testUser;
-	@Autowired
-	private UserController userController;
-	@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
-	@Autowired
-	private MockHttpServletRequest request;
+    private UserDto testUser;
+    @Autowired
+    private UserController userController;
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    @Autowired
+    private MockHttpServletRequest request;
 
-	@BeforeEach
-	void createContext() {
-		testUser = new UserDto(
-				1,
-				"user",
-				"user@yandex.ru"
-		);
-	}
+    @BeforeEach
+    void createContext() {
+        testUser = new UserDto(
+                1,
+                "user",
+                "user@yandex.ru"
+        );
+    }
 
-	@Test
-	void testBlankEmail() {
-		testUser.setEmail("");
+    @Test
+    void testBlankEmail() {
+        testUser.setEmail("");
 
-		ValidationException exception = assertThrows(
-				ValidationException.class,
-				() -> userController.create(testUser, request)
-		);
+        ValidationException exception = assertThrows(
+                ValidationException.class,
+                () -> userController.create(testUser, request)
+        );
 
-		assertEquals("Incorrect email " + testUser.getEmail(), exception.getMessage());
-	}
+        assertEquals("Incorrect email " + testUser.getEmail(), exception.getMessage());
+    }
 
-	@Test
-	void testEmailContainsSymbol() {
-		testUser.setEmail("memelover@gmail.com@");
+    @Test
+    void testEmailContainsSymbol() {
+        testUser.setEmail("memelover@gmail.com@");
 
-		ValidationException exception = assertThrows(
-				ValidationException.class,
-				() -> userController.create(testUser, request)
-		);
+        ValidationException exception = assertThrows(
+                ValidationException.class,
+                () -> userController.create(testUser, request)
+        );
 
-		assertEquals("Incorrect email " + testUser.getEmail(), exception.getMessage());
-	}
+        assertEquals("Incorrect email " + testUser.getEmail(), exception.getMessage());
+    }
 
-	@Test
-	void testBlankName() {
-		testUser.setName("");
+    @Test
+    void testBlankName() {
+        testUser.setName("");
 
-		ValidationException exception = assertThrows(
-				ValidationException.class,
-				() -> userController.create(testUser, request)
-		);
+        ValidationException exception = assertThrows(
+                ValidationException.class,
+                () -> userController.create(testUser, request)
+        );
 
-		assertEquals("Incorrect name " + testUser.getName(), exception.getMessage());
-	}
+        assertEquals("Incorrect name " + testUser.getName(), exception.getMessage());
+    }
 
-	@Test
-	void testNameContainsSpaces() {
-		testUser.setName("Super JavaProgrammer2000");
+    @Test
+    void testNameContainsSpaces() {
+        testUser.setName("Super JavaProgrammer2000");
 
-		ValidationException exception = assertThrows(
-				ValidationException.class,
-				() -> userController.create(testUser, request)
-		);
+        ValidationException exception = assertThrows(
+                ValidationException.class,
+                () -> userController.create(testUser, request)
+        );
 
-		assertEquals("Incorrect name " + testUser.getName(), exception.getMessage());
-	}
+        assertEquals("Incorrect name " + testUser.getName(), exception.getMessage());
+    }
 
-	@Test
-	void testDuplicateEmail() {
-		UserDto newUser = new UserDto(
-				2,
-				"user2",
-				"user@yandex.ru"
-		);
+    @Test
+    void testDuplicateEmail() {
+        UserDto newUser = new UserDto(
+                2,
+                "user2",
+                "user@yandex.ru"
+        );
 
-		userController.create(testUser, request);
+        userController.create(testUser, request);
 
-		ValidationException exception = assertThrows(
-				ValidationException.class,
-				() -> userController.create(newUser, request)
-		);
+        ValidationException exception = assertThrows(
+                ValidationException.class,
+                () -> userController.create(newUser, request)
+        );
 
-		assertEquals("Duplicate email " + newUser.getEmail(), exception.getMessage());
-	}
+        assertEquals("Duplicate email " + newUser.getEmail(), exception.getMessage());
+    }
 
-	@Test
-	void testUserWithWrongId(){
-		userController.create(testUser, request);
+    @Test
+    void testUserWithWrongId() {
+        userController.create(testUser, request);
 
-		UserNotFoundException exception = assertThrows(
-				UserNotFoundException.class,
-				() -> userController.getUserById(100, request)
-		);
+        UserNotFoundException exception = assertThrows(
+                UserNotFoundException.class,
+                () -> userController.getUserById(100, request)
+        );
 
-		assertEquals("User was not found", exception.getMessage());
-	}
+        assertEquals("User was not found", exception.getMessage());
+    }
 
-	@Test
-	void patchUser(){
-		UserDto updatedUser = new UserDto(
-				1,
-				"update",
-				null
-		);
+    @Test
+    void patchUser() {
+        UserDto updatedUser = new UserDto(
+                1,
+                "update",
+                null
+        );
 
-		userController.create(testUser, request);
+        userController.create(testUser, request);
 
-		userController.update(updatedUser,1,request);
+        userController.update(updatedUser, 1, request);
 
-		UserDto userAfterUpdate = userController.getUserById(testUser.getId(), request);
+        UserDto userAfterUpdate = userController.getUserById(testUser.getId(), request);
 
-		assertEquals(userAfterUpdate.getName(), updatedUser.getName());
-	}
+        assertEquals(userAfterUpdate.getName(), updatedUser.getName());
+    }
 
-	@Test
-	void patchUserEmailExists(){
-		UserDto updatedUser = new UserDto(
-				1,
-				null,
-				"user@user.ru"
-		);
-		UserDto testUser2 = new UserDto(
-				2,
-				"user",
-				"user@user.ru"
-		);
+    @Test
+    void patchUserEmailExists() {
+        UserDto updatedUser = new UserDto(
+                1,
+                null,
+                "user@user.ru"
+        );
+        UserDto testUser2 = new UserDto(
+                2,
+                "user",
+                "user@user.ru"
+        );
 
-		userController.create(testUser, request);
-		userController.create(testUser2, request);
+        userController.create(testUser, request);
+        userController.create(testUser2, request);
 
-		UserAlreadyExistsException exception = assertThrows(
-				UserAlreadyExistsException.class,
-				() -> userController.update(updatedUser, 1, request)
-		);
+        UserAlreadyExistsException exception = assertThrows(
+                UserAlreadyExistsException.class,
+                () -> userController.update(updatedUser, 1, request)
+        );
 
-		assertEquals("User with provided email " + updatedUser.getEmail() + " already exists", exception.getMessage());
-	}
+        assertEquals("User with provided email " + updatedUser.getEmail() + " already exists", exception.getMessage());
+    }
 
-	@Test
-	void testGetUserById(){
-		UserDto testUser2 = new UserDto(
-				2,
-				"user",
-				"user@user.ru"
-		);
-		userController.create(testUser, request);
-		UserDto user2 = userController.create(testUser2, request);
-		userController.getUserById(user2.getId(), request);
+    @Test
+    void testGetUserById() {
+        UserDto testUser2 = new UserDto(
+                2,
+                "user",
+                "user@user.ru"
+        );
+        userController.create(testUser, request);
+        UserDto user2 = userController.create(testUser2, request);
+        userController.getUserById(user2.getId(), request);
 
-		assertEquals(2, user2.getId());
-	}
+        assertEquals(2, user2.getId());
+    }
 
-	@Test
-	public void testDeleteUser() {
-		UserDto user = userController.create(testUser, request);
-		int actualSizeList = userController.getUsers().size();
-		assertEquals(1, actualSizeList);
+    @Test
+    public void testDeleteUser() {
+        UserDto user = userController.create(testUser, request);
+        int actualSizeList = userController.getUsers().size();
+        assertEquals(1, actualSizeList);
 
-		userController.delete(user.getId(), request);
-		actualSizeList = userController.getUsers().size();
+        userController.delete(user.getId(), request);
+        actualSizeList = userController.getUsers().size();
 
-		assertEquals(0, actualSizeList);
-	}
+        assertEquals(0, actualSizeList);
+    }
 }
