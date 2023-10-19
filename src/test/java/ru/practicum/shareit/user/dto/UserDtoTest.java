ccc7b7bdbfffaf22437f62e42be720552885bca5
DunaevDS <dmitryesfo@yandex.ru>
2023-10-19 21:55:29 +0500

    fix: test

--- src/test/java/ru/practicum/shareit/user/dto/UserDtoTest.java
+++ src/test/java/ru/practicum/shareit/user/dto/UserDtoTest.java
@@ -7,11 +7,6 @@ import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.json.JsonContent;
 
-import javax.validation.ConstraintViolation;
-import javax.validation.Validation;
-import javax.validation.Validator;
-import javax.validation.ValidatorFactory;
-import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,12 +15,10 @@ public class UserDtoTest {
 
     private final JacksonTester<UserDto> json;
     private UserDto userDto;
-    private final Validator validator;
+
 
     public UserDtoTest(@Autowired JacksonTester<UserDto> json) {
         this.json = json;
-        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-        validator = factory.getValidator();
     }
 
     @BeforeEach
@@ -33,7 +26,7 @@ public class UserDtoTest {
         userDto = new UserDto(
                 1,
                 "Apollon",
-                "bestJavaProgrammer@yandex.ru"
+                "apollo@yandex.ru"
         );
     }
 
@@ -44,52 +37,6 @@ public class UserDtoTest {
 
         assertThat(result).extractingJsonPathNumberValue("$.id").isEqualTo(1);
         assertThat(result).extractingJsonPathStringValue("$.name").isEqualTo("Apollon");
-        assertThat(result).extractingJsonPathStringValue("$.email").isEqualTo("bestJavaProgrammer@yandex.ru");
-    }
-
-    @Test
-    void test_UserDtoIsValid() {
-        Set<ConstraintViolation<UserDto>> violations = validator.validate(userDto);
-        assertThat(violations).isEmpty();
-    }
-
-    @Test
-    void test_UserDtoNameIsBlank() {
-        userDto.setName(" ");
-        Set<ConstraintViolation<UserDto>> violations = validator.validate(userDto);
-        assertThat(violations).isNotEmpty();
-        assertThat(violations.toString()).contains("interpolatedMessage='не должно быть пустым'");
-    }
-
-    @Test
-    void test_UserDtoNameIsNull() {
-        userDto.setName(null);
-        Set<ConstraintViolation<UserDto>> violations = validator.validate(userDto);
-        assertThat(violations).isNotEmpty();
-        assertThat(violations.toString()).contains("interpolatedMessage='не должно быть пустым'");
-    }
-
-    @Test
-    void test_UserDtoEmailIsBlank() {
-        userDto.setEmail(" ");
-        Set<ConstraintViolation<UserDto>> violations = validator.validate(userDto);
-        assertThat(violations).isNotEmpty();
-        assertThat(violations.toString()).contains("interpolatedMessage='не должно быть пустым'");
-    }
-
-    @Test
-    void test_UserDtoEmailNotEmail() {
-        userDto.setEmail("yandex@@@.yandex");
-        Set<ConstraintViolation<UserDto>> violations = validator.validate(userDto);
-        assertThat(violations).isNotEmpty();
-        assertThat(violations.toString()).contains("interpolatedMessage='должно иметь формат адреса электронной почты'");
-    }
-
-    @Test
-    void test_UserDtoEmailIsNull() {
-        userDto.setEmail(null);
-        Set<ConstraintViolation<UserDto>> violations = validator.validate(userDto);
-        assertThat(violations).isNotEmpty();
-        assertThat(violations.toString()).contains("interpolatedMessage='не должно быть пустым'");
+        assertThat(result).extractingJsonPathStringValue("$.email").isEqualTo("apollo@yandex.ru");
     }
 }
