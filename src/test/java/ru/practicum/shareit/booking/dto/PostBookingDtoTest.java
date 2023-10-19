aa82c398cb9414f45c4c2dd082d9642df2c83d27
DunaevDS <dmitryesfo@yandex.ru>
2023-10-19 21:39:33 +0500

    fix: checkstyle

--- src/test/java/ru/practicum/shareit/booking/dto/PostBookingDtoTest.java
+++ src/test/java/ru/practicum/shareit/booking/dto/PostBookingDtoTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 
 
-
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
@@ -19,22 +18,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class PostBookingDtoTest {
     private PostBookingDto postBookingDto;
     private Validator validator;
+
     @BeforeEach
     void beforeEach() {
         postBookingDto = new PostBookingDto(
                 1,
-                LocalDateTime.of(2030,12,25,12,0),
-                LocalDateTime.of(2030,12,26,12,0)
+                LocalDateTime.of(2030, 12, 25, 12, 0),
+                LocalDateTime.of(2030, 12, 26, 12, 0)
         );
 
         ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
         validator = factory.getValidator();
     }
+
     @Test
     void test_JsonBookingInputDto() {
         assertThat(postBookingDto.getItemId()).isEqualTo(1);
-        assertThat(postBookingDto.getStart()).isEqualTo(LocalDateTime.of(2030,12,25,12,0));
-        assertThat(postBookingDto.getEnd()).isEqualTo(LocalDateTime.of(2030,12,26,12,0));
+        assertThat(postBookingDto.getStart()).isEqualTo(LocalDateTime.of(2030, 12, 25, 12, 0));
+        assertThat(postBookingDto.getEnd()).isEqualTo(LocalDateTime.of(2030, 12, 26, 12, 0));
     }
 
     @Test
