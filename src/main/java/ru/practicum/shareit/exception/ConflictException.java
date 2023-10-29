d6fe9c24f07237f50cd73fc625c15fb1b7d5ed31
DunaevDS <dmitryesfo@yandex.ru>
2023-10-29 16:23:34 +0500

    feat: realized server\gateway structure

--- src/main/java/ru/practicum/shareit/exception/ConflictException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class ConflictException extends IllegalArgumentException {
-    public ConflictException(String message) {
-        super(message);
-    }
-}
--- src/main/java/ru/practicum/shareit/exception/InternalServerErrorException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class InternalServerErrorException extends IllegalArgumentException {
-    public InternalServerErrorException(String message) {
-        super(message);
-    }
-}
